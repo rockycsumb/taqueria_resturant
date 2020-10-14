890ba895face99a1b27064ce8278e48b47cc7e3d
rockycsumb <romoreno@csumb.edu>
2020-10-14 18:52:56 +0000

    .git fix

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-
+ 
 if(process.env.NODE_ENV === 'production'){
 	app.use(express.static('client/build'));
 	app.get('*', (req, res)=>{
