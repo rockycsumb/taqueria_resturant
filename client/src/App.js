7e2fe949430436a65f9f73c4bd39c4ca8cf16a67
rockycsumb <romoreno@csumb.edu>
2020-10-14 22:47:45 +0000

    added set up files

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import Navbar from './components/Navbar';
+import {BrowserRouter as Router} from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+    <Router>
+      <Navbar />
+    </Router>
   );
 }
 
